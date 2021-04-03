5d83893bdd312daefda607db735ce4aa444e33df
pavan tiruvuri <pavan.tiruvuri@oracle.com>
2021-04-03 12:53:02 +0530

    initialized project with maven quickstart

---
+++ library-extension/src/main/java/com/gql/App.java
@@ -0,0 +1,15 @@
+package com.gql;
+
+import graphql.schema.GraphQLSchema;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
