149f9e7948566e3f1c9dc53d780a14308159c0c7
pavan tiruvuri <pavan.tiruvuri@oracle.com>
2021-04-10 13:35:07 +0530

    Seeded JUnits and appplication public apis

--- library-extension/src/main/java/com/gql/App.java
+++ library-extension/src/main/java/com/gql/App.java
@@ -1,15 +1,115 @@
 package com.gql;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.gql.graphql.schema.idl.RuntimeWiring;
+import com.gql.graphql.schema.idl.SchemaGenerator;
+import com.gql.graphql.schema.idl.SchemaParser;
+import com.gql.graphql.schema.idl.SchemaPrinter;
+import com.gql.graphql.schema.idl.TypeDefinitionRegistry;
+import com.gql.graphql.schema.idl.errors.SchemaProblem;
+
 import graphql.schema.GraphQLSchema;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final String starWarsSchema = "library-extension/src/test/resources/starWarsSchema.graphqls";
+    private static final String blogSchema = "library-extension/src/test/resources/blogSchema.graphqls";
+
+    private RuntimeWiring buildRuntimeWiring() {
+        return RuntimeWiring.newRuntimeWiring().build();
+    }
+
+    public GraphQLSchema parseSchema(String fileContents) {
+
+        SchemaParser schemaParser = new SchemaParser();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
+        try {
+            typeRegistry.merge(schemaParser.parse(fileContents));
+        } catch (SchemaProblem e) {
+            e.printStackTrace();
+        }
+        RuntimeWiring wiring = buildRuntimeWiring();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, wiring);
+        return graphQLSchema;
+    }
+
+    public GraphQLSchema parseSchema(File file) {
+
+        SchemaParser schemaParser = new SchemaParser();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
+        try {
+            typeRegistry.merge(schemaParser.parse(readFile(file)));
+        } catch (SchemaProblem e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        RuntimeWiring wiring = buildRuntimeWiring();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, wiring);
+        return graphQLSchema;
+    }
+
+    public String printSchema(GraphQLSchema gqlSchema) {
+        SchemaPrinter schemaPrinter = new SchemaPrinter();
+        return schemaPrinter.print(gqlSchema);
+    }
+
+    private String readFile(File file) throws IOException {
+        String fileAsString = null;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = br.readLine();
+            }
+            fileAsString = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null)
+                br.close();
+        }
+        return fileAsString;
+    }
+
+    public String getFileContents(String filePath) {
+        String fileAsString = null;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = br.readLine();
+            }
+            fileAsString = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileAsString;
+    }
+
+    public static void main(final String[] args) {
+        App app = new App();
+        SchemaPrinter sp = new SchemaPrinter();
+        GraphQLSchema gql = null;
+        try {
+            gql = app.parseSchema(app.getFileContents(starWarsSchema));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String schemaTest = sp.print(gql);
+        System.out.println("Schema is: " + schemaTest);
+
     }
 }
--- library-extension/src/test/java/com/gql/AppTest.java
+++ library-extension/src/test/java/com/gql/AppTest.java
@@ -1,5 +1,12 @@
 package com.gql;
 
+import static org.junit.Assume.assumeFalse;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import graphql.schema.GraphQLSchema;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +14,86 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    String path = "src/test/resources/";
+    private static final String starWarsSchema = "starWarsSchema.graphqls";
+    private static final String blogSchema = "blogSchema.graphqls";
+
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    public String getFileContents(String filePath) {
+        String fileAsString = null;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = br.readLine();
+            }
+            fileAsString = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileAsString;
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    /**
+     * Tests for Resource "starWarsSchema.graphqls" under src/test
+     */
+    @org.junit.Test
+    public void testForResourceStarWars() {
+        File file = new File(path + starWarsSchema);
+        String absolutePath = file.getAbsolutePath();
+        System.out.println(absolutePath);
+
+        assertTrue(absolutePath.endsWith(starWarsSchema));
+    }
+
+    /**
+     * Tests for Resource "blogSchema.graphqls" under src/test
+     */
+    @org.junit.Test
+    public void testForResourceBlog() {
+        File file = new File(path + blogSchema);
+        String absolutePath = file.getAbsolutePath();
+
+        assertTrue(absolutePath.endsWith(blogSchema));
+    }
+
+    @org.junit.Test
+    public void testParseStarWarsSchema() {
+        App app = new App();
+        GraphQLSchema gqlSchema = app.parseSchema(getFileContents(path + starWarsSchema));
+        assertNotNull(gqlSchema);
+    }
+
+    @org.junit.Test
+    public void testParseBlogSchema() {
+        App app = new App();
+        GraphQLSchema gqlSchema = app.parseSchema(getFileContents(path + blogSchema));
+        assertNotNull(gqlSchema);
     }
 }
